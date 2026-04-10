92637347e2c476eaaf9a5149c2ae99361bd8da2f
www-angZuxi <juiceboy327@gmail.com>
2026-04-11 03:24:42 +0800

    fix: added css import in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import 'src/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
